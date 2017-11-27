b14c132e0e6ccf426d4c82397a79fecb53ccc4e3
Dog <xixixi_0987@hotmail.com>
2017-11-27 14:27:46 +0800

    1

--- python/test.py
+++ python/test.py
@@ -2,41 +2,44 @@ import RPi.GPIO
 import time
 import datetime
 
-#setup input pin
-InputA=16
-InputB=12
-TimeA=0
-TimeB=0
+#setup input pin,edit here
+InputA=16  #set GPIO16 as inputA
+InputB=12  #set pin GPIO12 as inputB
 
+# set up BCM GPIO numbering 
 RPi.GPIO.setmode(RPi.GPIO.BCM)
 
-#pull up pins to high as default
-RPi.GPIO.setup(InputA, RPi.GPIO.IN, pull_up_down=RPi.GPIO.PUD_UP) 
-RPi.GPIO.setup(InputB, RPi.GPIO.IN, pull_up_down=RPi.GPIO.PUD_UP) 
+#set pins to input mode and pull up pins to high(if need) as default
+#RPi.GPIO.setup(InputA, RPi.GPIO.IN, pull_up_down=RPi.GPIO.PUD_UP)
+RPi.GPIO.setup(InputA, RPi.GPIO.IN) 
+RPi.GPIO.setup(InputB, RPi.GPIO.IN) 
 
 # Define a threaded callback function to run in another thread when events are detected  
-def writeTimeA():  
+def writeTimeA(channel):   
+        TimeA=0
 	if RPi.GPIO.input(InputA):
-                TimeA= time.time() #record time A   
-                print ("TimeA= %s"%(TimeA))  
-                return TimeA
+            TimeA= time.time() #record time A   
+            print ("TimeA= %s"%(TimeA))  
+        return TimeA
     
 
-def writeTimeB():  
-	if RPi.GPIO.input(InputB)==1:
-    		TimeB= time.time() #record time B   
-        	print ("TimeB= %s"%(TimeB))  
-        	return TimeB
+def writeTimeB(channel):
+        TimeB=0
+	if RPi.GPIO.input(InputB):
+    	    TimeB= time.time() #record time B   
+            print ("TimeB= %s"%(TimeB))  
+        return TimeB
 
 #detect rising edge
-RPi.GPIO.add_event_detect(InputA, RPi.GPIO.RISING, callback=writeTimeA)
-RPi.GPIO.add_event_detect(InputB, RPi.GPIO.RISING, callback=writeTimeB) 
+RPi.GPIO.add_event_detect(InputA, RPi.GPIO.RISING, callback=writeTimeA, bouncetime=100)
+RPi.GPIO.add_event_detect(InputB, RPi.GPIO.RISING, callback=writeTimeB,bouncetime=100) 
 
 try:  
     while True:
-            time.sleep(0.1)
-            s = writeTimeB()-writeTimeA()
-            if s !=0:
+            time.sleep(0.2)
+            print ("InputA= %s") %(RPi.GPIO.input(InputA))
+            #s = writeTimeB(1)- writeTimeA(1)
+            if writeTimeB(12)!=0 and writeTimeA(16)!=0:
             	print ("TimeB-TimeA= %d")%(s)
             	print ("-----------------")
             
