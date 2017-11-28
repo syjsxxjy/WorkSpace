fdd7b938a1fbf1b373d809dc45a5f1f5658d937a
Dog <xixixi_0987@hotmail.com>
2017-11-28 14:08:35 +0800

    1

--- python/test.py
+++ python/test.py
@@ -2,6 +2,8 @@
 #-----------------------------------------------------------------#
 # "#"<- this mark is comments, codes after "#" will not be run.   #
 #-----------------------------------------------------------------#
+#RPi.GPIO.wait_for_edge(InputA, RPi.GPIO.RISING,bouncetime=200)
+#
 
 #import libraries
 import RPi.GPIO
@@ -12,48 +14,73 @@ import datetime
 InputA=16  #set GPIO16 as inputA
 InputB=7   #set GPIO7 as inputB
 
+#setup output pin for LED
+LED=12
+
 # set up BCM GPIO numbering 
 RPi.GPIO.setmode(RPi.GPIO.BCM)
 
 #set pins to input mode and pull up pins to high(if need) as default
 RPi.GPIO.setup(InputA, RPi.GPIO.IN) 
-RPi.GPIO.setup(InputB, RPi.GPIO.IN) 
+RPi.GPIO.setup(InputB, RPi.GPIO.IN)
+
+#set pin for LED to output mode
+RPi.GPIO.setup(LED, RPi.GPIO.OUT)
 
 #set up variables to save start and end time, initialize them to 0 as default
-TimeA=0   
-TimeB=0   
+TimeA=0         
+TimeB=0
 
-# Define a threaded callback function to run in another thread when events are detected  
-def writeTimeA(channel):   
-        #TimeA=0
+# Define serveral threaded callbacks function to run in another thread when events are detected
+# 
+def writeTimeA():   
     if RPi.GPIO.input(InputA):
             global TimeA
             TimeA= time.time() #record time A   
-            print ("TimeA= %s"%(TimeA))  
-        return TimeA
-    
+            print ("TimeA in writeTimeA= %s"%(TimeA))
 
-def writeTimeB(channel):
-        #TimeB=0
+def writeTimeB():
     if RPi.GPIO.input(InputB):
+            global TimeB
             TimeB= time.time() #record time B   
-            print ("TimeB= %s"%(TimeB))  
-        return TimeB
+            print ("writeTimeB= %s"%(TimeB))           
+            
+def turnon():
+        RPi.GPIO.output(LED, True)
+        print ("Output = True")
+        time.sleep(0.4)
 
+def turnoff():
+        RPi.GPIO.output(LED, False)
+
+def CountA(channel):
+        if RPi.GPIO.input(InputA):
+            writeTimeA()
+        if RPi.GPIO.event_detected(InputB):
+            writeTimeB()
+        global TimeB
+        global TimeA
+        s=TimeB*100-TimeA*100
+        print ("s= %d")%(s)
+        if 0<s<200:
+           turnon()
+           turnoff()       
+        TimeA = -1
+        TimeB = -1        
+        
 #detect rising edge
-RPi.GPIO.add_event_detect(InputA, RPi.GPIO.RISING, callback=writeTimeA, bouncetime=100)
-RPi.GPIO.add_event_detect(InputB, RPi.GPIO.RISING, callback=writeTimeB, bouncetime=100) 
+RPi.GPIO.add_event_detect(InputA, RPi.GPIO.RISING, callback=CountA,bouncetime=200 ) #bouncetime=200
+RPi.GPIO.add_event_detect(InputB, RPi.GPIO.RISING,callback=CountA,bouncetime=200 )
 
 try:  
     while True:
             time.sleep(0.2)
             print ("InputA= %s") %(RPi.GPIO.input(InputA))
+            print ("InputB= %s") %(RPi.GPIO.input(InputB))
             print ("Main: TimeA= %s") %(TimeA)
-            #print ("writeTimeA= %s") %(writeTimeA(16))
-            # s = writeTimeB(12)- writeTimeA(16)
-            #if writeTimeB(12)!=0 and writeTimeA(16)!=0:
-            #   print ("TimeB-TimeA= %d")%(s)
-            #   print ("-----------------")
+            print ("Main: TimeB= %s") %(TimeB)
+            #print ("TimeB-TimeA= %d")%(s)
+            print ("-----------------")
             
   
 finally:                   # this block will run no matter how the try block exits  
